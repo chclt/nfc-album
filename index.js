6aec6d5a0dc3e7674b716217d36ea38baea87616
Chclt <meakingchan@gmail.com>
2023-02-22 00:46:55 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// import '@material/web/button/elevated-button.js';
+// import '@material/web/button/elevated-link-button.js';
+// import '@material/web/button/filled-button.js';
+// import '@material/web/button/filled-link-button.js';
+// import '@material/web/button/outlined-button.js';
+// import '@material/web/button/outlined-link-button.js';
+import '@material/web/button/text-button.js';
+import '@material/web/button/text-link-button.js';
+import '@material/web/button/tonal-button.js';
+import '@material/web/button/tonal-link-button.js';
+
+import '@material/web/checkbox/checkbox.js';
+
+import '@material/web/dialog/dialog.js';
+import '@material/web/divider/divider.js';
+
+// import '@material/web/field/filled-field.js';
+// import '@material/web/field/outlined-field.js';
+// import '@material/web/focus/focus-ring.js';
+// import '@material/web/focus/strong-focus.js';
+
+import '@material/web/list/list.js';
+import '@material/web/list/list-item.js';
+
+import '@material/web/radio/radio.js';
+import '@material/web/ripple/ripple.js';
+
+import '@material/web/switch/switch.js';
+import '@material/web/textfield/filled-text-field.js';
+import '@material/web/textfield/outlined-text-field.js';
